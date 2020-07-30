5c3e47558afdb30af91676e6dfbbc6d2ca686ce2
agusrichard <agus.richard21@gmail.com>
2020-07-30 11:18:36 +0700

    Type Basics, Objects, Arrays, Explicit Types

--- net-ninja/sandbox.ts
+++ net-ninja/sandbox.ts
@@ -2,29 +2,38 @@
 // let sekar = "Sekardayu Hana Pradiani"
 // let areYouOk = false
 // let diameter = 21
+
 // const circ = (diameter: number) => {
 //   return diameter * Math.PI
 // }
+
+
 // console.log(circ(diameter))
+
+
 // ==================== Objects and Arrays ====================
 // let arr0 = [1, 2, 3, 4, 5]
 // console.log('arr0', arr0)
 // arr0.push(5)
 // // arr.push("sekar")
 // // arr0[0] = 'saskia'
+
 // let arr1 = ["sekar", "saskia", "sintya"]
 // console.log('arr1', arr1)
 // arr1.push('arifa')
 // // arr1.push(0)
 // // arr1[0] = 0
+
 // let mixed = [0, 1, 'sekar', 2, 'saskia', 4, 5]
 // console.log('mixed', mixed)
 // mixed.push('sintya')
 // mixed.push(0)
+
 // let person = {
 //   name: 'Agus',
 //   age: 22
 // }
+
 // // person = "soemthing"
 // // person.age = '22'
 // // person.newAtt = 'something'
@@ -33,21 +42,34 @@
 // //   age: 22,
 // //   friends: ['sekar', 'saskia']
 // // }
+
+
 // ==================== Explicit Types ====================
-var sekar;
-var age;
-var amIGood;
-console.log(sekar, age, amIGood);
-var names;
-names.push('sekar');
-names.push('saskia');
+let sekar: string
+let age: number
+let amIGood: boolean
+
+console.log(sekar, age, amIGood)
+
+let names: string[]
+names.push('sekar')
+names.push('saskia')
 // names.push(20)
-var mixedArr = [];
-mixedArr.push('sekar');
-mixedArr.push(20);
-var uid = '';
+
+let mixedArr: (string|number)[] = []
+mixedArr.push('sekar')
+mixedArr.push(20)
+
+let uid: string|number = ''
+
 // objects
-var ninjaOne;
+let ninjaOne: object;
 ninjaOne = { name: 'yoshi', age: 30 };
-var ninjaTwo;
+
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string
+};
 ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
+
