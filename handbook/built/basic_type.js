b01a0f1a5d7209b52b327dae9d0ef4ecc21d526e
agusrichard <agus.richard21@gmail.com>
2021-03-27 18:18:45 +0700

    Somekind of typescript

---
+++ handbook/built/basic_type.js
@@ -0,0 +1,7 @@
+var sekar = 'Sekardayu Hana Pradiani';
+var saskia = 'Saskia Nurul Azhima';
+var age = 23;
+function printName(name) {
+    console.log(name.toLowerCase());
+}
+printName(sekar);
---
+++ handbook/src/basic_type.ts
@@ -0,0 +1,9 @@
+const sekar: string = 'Sekardayu Hana Pradiani'
+const saskia: string = 'Saskia Nurul Azhima'
+const age: number = 23
+
+function printName(name: string): void {
+    console.log(name.toLowerCase())
+}
+
+printName(sekar)
