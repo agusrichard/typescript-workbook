c422cec40edfd3baa7ccce9d343f83815ad0d174
agusrichard <agus.richard21@gmail.com>
2021-09-21 21:00:10 +0700

    The configuration has not finished yet

---
+++ wallet/dist/server.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.listen(() => {
+    console.log(`Server listening on port ${port}`);
+});
+//# sourceMappingURL=server.js.map
--- wallet/src/app.ts
+++ wallet/src/app.ts
@@ -1,11 +1,11 @@
+
 import express from 'express'
+import routes from './routes/index'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.use(routes)
 
 app.listen(() => {
     console.log(`Server listening on port ${port}`)
