5a27daf01f3b85788616c688943dc4b7830b1685
agusrichard <agus.richard21@gmail.com>
2021-10-08 15:07:35 +0700

    Load testing with k6 initial

---
+++ load-testing/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('Hello World!');
+console.log('Lets load testing with k6');
+//# sourceMappingURL=index.js.map
---
+++ load-testing/src/index.ts
@@ -0,0 +1,2 @@
+console.log('Hello World!')
+console.log('Lets load testing with k6')
